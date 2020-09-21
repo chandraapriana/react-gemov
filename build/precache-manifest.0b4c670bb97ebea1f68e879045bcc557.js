927553341020768525d9a0d468e71686f843abae
Chandra Muhamad Apriana <50396186+chandraapriana@users.noreply.github.com>
2020-09-21 12:22:25 +0300

    fixing 2

---
+++ build/precache-manifest.0b4c670bb97ebea1f68e879045bcc557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86375eeb3327d269162d3d9d39e9839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45254527b09056c0a512",
+    "url": "/static/css/main.81336a63.chunk.css"
+  },
+  {
+    "revision": "eea2c92e60f4103d728b",
+    "url": "/static/js/2.781349a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.781349a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45254527b09056c0a512",
+    "url": "/static/js/main.602907a1.chunk.js"
+  },
+  {
+    "revision": "a08d91d0055db3ef052d",
+    "url": "/static/js/runtime-main.90b10edd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
